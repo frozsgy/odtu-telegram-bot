fcd945cb649491b4d2b3b638917296790e7b423e
Mustafa Ozan Alpay <frozsgy@gmail.com>
2022-09-30 16:09:16 +0300

    Enable logging for driver.py

--- driver.py
+++ driver.py
@@ -1,21 +1,22 @@
+import logging
+import sys
+import time
+
 from bot import *
 
-import time
-import sys
-
-verbose = False
-logging = False
+is_verbose = False
+logging_enabled = False
 
 for i in sys.argv:
     if i in ('--verbose', '-V'):
-        verbose = True
+        is_verbose = True
     if i in ('--log', '-L'):
-        logging = True
+        logging_enabled = True
 
-b = Bot(verbose, logging)
+b = Bot(is_verbose, logging_enabled)
 
 
-def run_bot(verbose=False, logging=False):
+def run_bot(verbose=False):
     start_time = time.time()
     while True:
         try:
@@ -24,18 +25,21 @@ def run_bot(verbose=False, logging=False):
             time.sleep(1.0 - ((time.time() - start_time) % 1.0))
         except requests.exceptions.ConnectionError as e:
             if verbose is True:
-                print("Connection error: ", e)
-            print("Waiting for internet connection...")
+                logging.critical("Connection error: ", e)
+            logging.info("Waiting for internet connection...")
             time.sleep(5.0)
         except KeyboardInterrupt:
-            print("See y'all folks!")
+            logging.info("See y'all folks!")
             exit()
 
 
 if __name__ == "__main__":
     b.hello()
-    if verbose is True:
-        print("Verbose mode is active! You will see plenty of stdout.")
-    if logging is True:
-        print("Logging is active! Messages will be logged to the database.")
+    if is_verbose is True:
+        logging.basicConfig(level=logging.DEBUG)
+        logging.info("Verbose mode is active! You will see plenty of debug information.")
+    else:
+        logging.basicConfig(level=logging.INFO)
+    if logging_enabled is True:
+        logging.info("Logging is active! Messages will be logged to the database.")
     run_bot()
