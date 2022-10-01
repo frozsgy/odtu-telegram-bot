f64688fde421b94bba87353385b3e7acbeeb1969
Mustafa Ozan Alpay <frozsgy@gmail.com>
2022-10-02 00:38:38 +0300

    Add color support for logger

--- logger.py
+++ logger.py
@@ -1,4 +1,10 @@
 import logging
 
 logger = logging
+logger.addLevelName(logging.CRITICAL, "\033[1;41m%s\033[1;0m" % logging.getLevelName(logging.CRITICAL))
+logger.addLevelName(logging.ERROR, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.ERROR))
+logger.addLevelName(logging.WARNING, "\033[1;33m%s\033[1;0m" % logging.getLevelName(logging.WARNING))
+logger.addLevelName(logging.INFO, "\033[1;32m%s\033[1;0m" % logging.getLevelName(logging.INFO))
+logger.addLevelName(logging.DEBUG, "\033[1;35m%s\033[1;0m" % logging.getLevelName(logging.DEBUG))
+
 logger.basicConfig(format='[%(levelname)s:%(asctime)s] %(message)s', level=logging.INFO)
